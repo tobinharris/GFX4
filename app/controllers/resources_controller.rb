da85f420ed12fa1f95611b463946d29605ec8346
tobinharris <tobin@tobinharris.com>
2009-09-21 22:07:35 +0100

    Slightly better random selection.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,8 +1,11 @@
 class ResourcesController < ApplicationController
+  def page_size
+    9
+  end
   
   def search
-    @resources = Resource.find_with_index( params[:id] || params[:query], {:limit=>20})   
-    @resources = @resources | Resource.find_tagged_with( params[:id] || params[:query])
+    @resources = Resource.find_with_index( params[:id] || params[:query], {:limit=>page_size})   
+    @resources = @resources | Resource.find_tagged_with( params[:id] || params[:query], :limit=>page_size)
     params[:query]= params[:id]
     respond_to do |format|
       format.html {render :action => 'index'}
@@ -16,14 +19,10 @@ class ResourcesController < ApplicationController
   def index 
     
     if not params[:id].nil? then
-      @resources =  Resource.find_with_index( params[:id], {:limit=>20} )             
+      @resources =  Resource.find_with_index( params[:id], {:limit=>page_size} )             
       params[:query] = params[:id]
     else
-        #@resources = Resource.find(:all, :conditions=>{:hot_topic=>'show_off'}, :offset=>0, :limit=>20, :order=>'created_at asc')
-#        favored = ['yoga','characters','roadsign','painting','spice','chess']
- #       tag = favored[rand(favored.length)]
-        @resources = [] #Resource.find_with_index('characters', {:limit=>3, :order=>'name'})      
-        
+        @resources = Resource.random_selection(3)
     end
     
     @resource_count = Resource.count
@@ -35,18 +34,15 @@ class ResourcesController < ApplicationController
   end
 
   def show
-    @resource = Resource.find_by_url_hash(params[:id])
-
+    @resource = Resource.find_by_url_hash(params[:id])    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @resource }
     end
   end 
              
-  def random        
-    tag = Tag.find :first, :offset=>rand(Tag.count)
-    @resources = Resource.find_tagged_with(tag.to_s)
-    params[:query] = tag.to_s
+  def random           
+    @resources = Resource.random_selection page_size, page_size        
     render :action=>'index'
   end
   
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,24 @@
 class Resource < ActiveRecord::Base
   acts_as_indexed :fields => [:name, :tag_list]    
   acts_as_taggable
+   
+  def self.random_tag
+    Tag.find :first, :offset=>rand(Tag.count)  
+  end
+  
+  def self.random_selection(max=20, min=0)
+     tag = Resource.random_tag
+     resources = Resource.find_tagged_with(tag.to_s, :limit=>max)
+     
+     attempts = 0
+     while resources.length < max and attempts < 3
+       attempts = attempts + 1
+       resources = resources | Resource.random_selection(max - resources.length)
+     end                       
+     
+     resources
+  end
+  
   
   #want to set a hash that's unique despite the id
   def before_save
